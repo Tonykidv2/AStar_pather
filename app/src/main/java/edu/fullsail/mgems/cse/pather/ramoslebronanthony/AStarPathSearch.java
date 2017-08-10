61e3702910ccdb468ff1a9077b895aa04719f763
Anthony Ramoslebron <ajramoslebron@gmail.com>
2017-08-10 14:13:28 -0400

    Finished work on AStar class for now

--- app/src/main/java/edu/fullsail/mgems/cse/pather/ramoslebronanthony/AStarPathSearch.java
+++ app/src/main/java/edu/fullsail/mgems/cse/pather/ramoslebronanthony/AStarPathSearch.java
@@ -13,27 +13,127 @@ public class AStarPathSearch {
 
     ArrayList<SearchNode> m_Tiles;
     HashMap<NavCell, SearchNode> m_SearchGraph;
-
     Queue<PlannerNode> m_OpenList;
     HashMap<NavCell, PlannerNode> m_VisitedList;
 
+    PriorityQueue<PlannerNode> m_OpenEnhancer;
 
     SearchNode m_Start;
     SearchNode m_End;
 
     boolean m_FoundIt;
+    float hWeight = 1.2f;
 
-    PriorityQueue<PlannerNode> m_OpenEnhancer;
-
-
-    void Initaililizer(ArrayList<NavCell> _tileList[][], int Row, int Column)
+    void Init(NavCell _tileList[][], int Row, int Column)
     {
-        
+        m_Tiles = new ArrayList<SearchNode>();
+        m_SearchGraph = new HashMap<NavCell, SearchNode>();
+
+        for (int r = 0; r < Row; r++)
+        {
+            for (int c = 0; c < Column; c++)
+            {
+
+                SearchNode newNode = new SearchNode();
+
+                newNode.m_Cell = _tileList[r][c];
+
+                if(r == 0 && c == 0)
+                {
+                    newNode.m_Neighbors.add(_tileList[r][c+1]);
+                    newNode.m_Neighbors.add(_tileList[r +1][c]);
+                }
+                else if(r == 0 && c == Column)
+                {
+                    newNode.m_Neighbors.add(_tileList[r][c -1]);
+                    newNode.m_Neighbors.add(_tileList[r + 1][c]);
+                }
+                else if(r == Row && c == 0)
+                {
+                    newNode.m_Neighbors.add(_tileList[r][c + 1]);
+                    newNode.m_Neighbors.add(_tileList[r - 1][c]);
+                }
+                else if(c == 0)
+                {
+                    newNode.m_Neighbors.add(_tileList[r + 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r - 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r][c + 1]);
+                }
+                else if(r == 0)
+                {
+                    newNode.m_Neighbors.add(_tileList[r + 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r][c + 1]);
+                    newNode.m_Neighbors.add(_tileList[r][c - 1]);
+                }
+                else if(r == Row - 1)
+                {
+                    newNode.m_Neighbors.add(_tileList[r - 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r][c + 1]);
+                    newNode.m_Neighbors.add(_tileList[r][c - 1]);
+                }
+                else if(c == Column - 1)
+                {
+                    newNode.m_Neighbors.add(_tileList[r + 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r - 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r][c - 1]);
+                }
+                else
+                {
+                    newNode.m_Neighbors.add(_tileList[r][c + 1]);
+                    newNode.m_Neighbors.add(_tileList[r][c - 1]);
+                    newNode.m_Neighbors.add(_tileList[r + 1][c]);
+                    newNode.m_Neighbors.add(_tileList[r - 1][c]);
+                }
+
+                m_Tiles.add(newNode);
+            }
+        }
+
+        for (int i = 0; i < m_Tiles.size(); i++)
+        {
+            m_SearchGraph.put(m_Tiles.get(i).m_Cell, m_Tiles.get(i));
+        }
     }
 
     void Enter (NavCell Start, NavCell End)
     {
+        m_FoundIt = false;
 
+        for (int i = 0; i < m_Tiles.size(); i++)
+        {
+            if(m_Tiles.get(i).m_Cell == Start)
+            {
+                m_Start = m_Tiles.get(i);
+            }
+            if(m_Tiles.get(i).m_Cell == End)
+            {
+                m_End = m_Tiles.get(i);
+            }
+        }
+
+        PlannerNode starter = new PlannerNode();
+        starter.m_Parent = null;
+        starter.m_Vertex = m_Start;
+
+        m_OpenEnhancer = new PriorityQueue<PlannerNode>();
+        m_OpenEnhancer.add(starter);
+
+        m_VisitedList = new HashMap<NavCell, PlannerNode>();
+        m_VisitedList.put(m_Start.m_Cell, m_OpenEnhancer.peek());
+
+        m_OpenEnhancer.peek().m_HeuristicCost = Estimate(m_Start.m_Cell, m_End.m_Cell);
+        m_OpenEnhancer.peek().m_GivenCost = 0;
+        m_OpenEnhancer.peek().m_FinalCost = m_OpenEnhancer.peek().m_HeuristicCost * hWeight;
+    }
+
+    float Estimate(NavCell start, NavCell goal)
+    {
+        double x = (goal.getM_Center().x - start.getM_Center().x);
+        x *= x;
+        double y = (goal.getM_Center().y - start.getM_Center().y);
+        y *= y;
+
+        return (float)Math.sqrt(x + y);
     }
 
     boolean IsDone()
@@ -43,14 +143,82 @@ public class AStarPathSearch {
 
     void update(long _timeslice)
     {
+        long slicer = _timeslice;
 
+        while (true)
+        {
+            PlannerNode current = m_OpenEnhancer.peek();
+
+            m_OpenEnhancer.remove();
+
+
+            if(current.m_Vertex == m_End)
+            {
+                m_VisitedList.put(current.m_Vertex.m_Cell, current);
+                m_FoundIt = true;
+                slicer = 0;
+                return;
+            }
+
+            int size = current.m_Vertex.m_Neighbors.size();
+            for (int i = 0; i < size; i++)
+            {
+
+                if(current.m_Vertex.m_Neighbors.get(i).m_Weight == 0)
+                    continue;
+
+                float tempGivenCost = current.m_GivenCost + Estimate(current.m_Vertex.m_Cell, current.m_Vertex.m_Neighbors.get(i))
+                        * current.m_Vertex.m_Neighbors.get(i).m_Weight;
+
+                if(!m_VisitedList.containsKey(current.m_Vertex.m_Neighbors.get(i)))
+                {
+                    SearchNode successor = m_SearchGraph.get(current.m_Vertex.m_Neighbors.get(i));
+                    PlannerNode successorNode = new PlannerNode();
+
+                    successorNode.m_Vertex = successor;
+                    successorNode.m_Parent = current;
+                    successorNode.m_GivenCost = tempGivenCost;
+                    successorNode.m_HeuristicCost = Estimate(successor.m_Cell, m_End.m_Cell);
+                    successorNode.m_FinalCost = successorNode.m_GivenCost + successorNode.m_HeuristicCost
+                            * hWeight;
+
+                    m_VisitedList.put(current.m_Vertex.m_Neighbors.get(i), successorNode);
+                    m_OpenEnhancer.add(successorNode);
+                }
+
+                else
+                {
+                    if( tempGivenCost < m_VisitedList.get(current.m_Vertex.m_Neighbors.get(i)).m_GivenCost)
+                    {
+                        PlannerNode Succesor = m_VisitedList.get(current.m_Vertex.m_Neighbors.get(i));
+                        m_OpenEnhancer.remove(Succesor);
+                        Succesor.m_GivenCost = tempGivenCost;
+                        Succesor.m_FinalCost = Succesor.m_GivenCost + Succesor.m_HeuristicCost * hWeight;
+                        Succesor.m_Parent = current;
+                        m_OpenEnhancer.add(Succesor);
+                    }
+                }
+            }
+        }
     }
 
     ArrayList<NavCell> Solution()
     {
-        ArrayList<NavCell> result = new ArrayList<NavCell>();
+        ArrayList<NavCell> result = null; //new ArrayList<NavCell>();
 
+        PlannerNode walker = m_VisitedList.get(m_End.m_Cell);
 
+        if(walker != null)
+        {
+            result = new ArrayList<NavCell>();
+            result.add(walker.m_Vertex.m_Cell);
+            walker = walker.m_Parent;
+            while(walker != null)
+            {
+                result.add(walker.m_Vertex.m_Cell);
+                walker = walker.m_Parent;
+            }
+        }
 
         return result;
     }
--- app/src/main/java/edu/fullsail/mgems/cse/pather/ramoslebronanthony/NavCell.java
+++ app/src/main/java/edu/fullsail/mgems/cse/pather/ramoslebronanthony/NavCell.java
@@ -15,8 +15,11 @@ public class NavCell {
     private Rect m_Bounds;
     private Point m_Center;
 
-    NavCell (Rect _bounds, Point _center)
+    float m_Weight;
+
+    NavCell (Rect _bounds, Point _center, float _weight)
     {
+        m_Weight = _weight;
         m_Bounds = _bounds;
         m_Center = _center;
     }
@@ -29,4 +32,5 @@ public class NavCell {
     {
         return m_Center;
     }
+
 }
